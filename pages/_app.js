38d383b762741cc0f802ac6e51cdb9d39efc2ac0
tih95 <tristan.honda@gmail.com>
2020-08-12 19:26:09 -0700

    added a meta tag for viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,26 @@
 import Head from 'next/head';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
-import Header from '../components/Header.component'
+import Header from '../components/Header.component';
 import { customTheme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <React.Fragment>
-      <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-      </Head>
+	return (
+		<React.Fragment>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 
-      <ThemeProvider theme={customTheme}>
-        <CSSReset />
-        <Header />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
-  )
+			<ThemeProvider theme={customTheme}>
+				<CSSReset />
+				<Header />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</React.Fragment>
+	);
 }
 
-export default MyApp
+export default MyApp;
